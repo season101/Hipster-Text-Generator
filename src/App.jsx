97e332bf2fd6684c1293ca80a857630f023dc9d3
Sijan Malla <sijan.malla101@gmail.com>
2023-09-13 12:05:34 -0500

    Added HTML structure and functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,37 @@
+import { nanoid } from 'nanoid';
+import { useState } from 'react';
+import text from './data';
+
 const App = () => {
-  return <h2>Lorem Ipsum Starter</h2>;
+  const [count, setCount] = useState(1);
+  const [textArray, setTextArray] = useState([]);
+  const handleChange = (e) => {
+    if (e.target.value < 0 || e.target.value > 8) return;
+    setCount(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTextArray(text.slice(0, count));
+  };
+  return (
+    <>
+      <h2>Lorem Ipsum Starter</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="number"
+          name="number"
+          id="number"
+          value={count}
+          onChange={handleChange}
+        />
+        <button type="submit">Submit</button>
+      </form>
+      <section>
+        {textArray.map((textArr) => {
+          return <article key={nanoid()}>{textArray}</article>;
+        })}
+      </section>
+    </>
+  );
 };
 export default App;
